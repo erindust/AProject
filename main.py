7ae593d9ae790ff88a30d97ba906474d26a945d8
erindust <nelsonering@gmail.com>
2025-03-25 09:36:32 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+# for j in range(10):
+#     stars = ""
+#     for i in range(j):
+#         stars += "*" + " "
+#     print(stars)
+
+# for j in range(10):
+#     stars = ""
+#     for i in range(0,10,j+1):
+#         stars += "*" + " "
+#     print(stars)
+
+# for j in range(10):
+#     stars = ""
+#     for i in range(10,0,(j+1)*(-1)):
+#         stars += "*" + " "
+#     print(stars)
+    
+    
+    
+for i in range(3):
+    stars=""
+    for j in range(5):
+        stars += '*'
+    print(stars)
